c7589984ba7b6924c42f7afc3cb86934fb9ecb86
luissilva <luis.silva@slalom.com>
2024-07-19 15:37:27 +1000

    add opensearch serverless tf module

--- outputs.tf
+++ outputs.tf
@@ -6,15 +6,15 @@ output "tfc_workspace_name" {
 #Opensearch serverless collection output
 output "opensearch_serverless_collection_endpoint" {
     description = "Opensearch serverless collection endpoint"
-    value = aws_opensearchserverless_collection.kb_os_collection.collection_endpoint
+    value = module.opensearch_collection.endpoint
 }
 
 output "opensearch_serverless_collection_arn" {
     description = "Opensearch serverless collection arn"
-    value = aws_opensearchserverless_collection.kb_os_collection.arn
+    value = module.opensearch_collection.arn
 }
 
 output "opensearch_serverless_collection_id" {
     description = "Opensearch serverless collection id"
-    value = aws_opensearchserverless_collection.kb_os_collection.id
+    value = module.opensearch_collection.id
 }
