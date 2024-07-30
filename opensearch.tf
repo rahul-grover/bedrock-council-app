0a83afb11456dd5477524ef0a823f7208b66ecef
luisslalom <119266665+luisslalom@users.noreply.github.com>
2024-07-30 12:33:29 +1000

    Feature/iac (#35)
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * update provider version
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates
    
    * adding tf templates

--- opensearch.tf
+++ opensearch.tf
@@ -111,7 +111,7 @@ resource "aws_opensearchserverless_access_policy" "pipeline" {
       }
     ],
     Principal = [
-      data.aws_caller_identity.current.arn,
+      # data.aws_caller_identity.current.arn,
       "arn:aws:sts::${local.account_id}:assumed-role/${var.pipeline_iam_role}/*",
       "arn:aws:iam::${local.account_id}:role/${var.pipeline_iam_role}"
     ]
