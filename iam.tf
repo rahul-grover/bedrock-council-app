8e7025cc77688976558470dfd5501f4ad9154e4d
luisslalom <119266665+luisslalom@users.noreply.github.com>
2024-08-01 16:43:41 +1000

    Feature/awscc (#46)
    * fixing awscc
    
    * fixing awscc
    
    * adding parser for pre-processing prompt

--- iam.tf
+++ iam.tf
@@ -190,3 +190,25 @@ resource "aws_iam_role" "lambda" {
   managed_policy_arns = [data.aws_iam_policy.lambda_basic_execution.arn]
 }
 
+resource "aws_iam_role" "lambda_parser" {
+  name = "FunctionExecutionRoleForLambda_${var.agent_action_group}_lambda_parser"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = "${local.account_id}"
+          }
+        }
+      }
+    ]
+  })
+  managed_policy_arns = [data.aws_iam_policy.lambda_basic_execution.arn]
+}
+
