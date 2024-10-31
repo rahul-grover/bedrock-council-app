40971f7d3e3cf5ad31eae13bc277ed6fee2b8fd1
gunjan.mehta <gunjan.mehta@slalom.com>
2024-10-31 17:24:29 +1100

    updated instance name

--- ec2.tf
+++ ec2.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "bedrock_sg" {
 }
 
 
-resource "aws_instance" "bedrock_instance_test" {
+resource "aws_instance" "terraform_bedrock_instance" {
   ami             = var.ami_id
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.bedrock_sg.name]
