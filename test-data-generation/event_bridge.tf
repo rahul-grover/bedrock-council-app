7fc9e70b00500ea7287046d6e0d347c6df32dbc1
rahul grover <rahul.grover@prezzee.com>
2024-11-15 11:50:46 +1100

    Removing duplicate cloud_watch_event_rule

--- test-data-generation/event_bridge.tf
+++ test-data-generation/event_bridge.tf
@@ -20,23 +20,6 @@ resource "aws_cloudwatch_event_rule" "s3_file_upload" {
   })
 }
 
-
-# EventBridge rule
-resource "aws_cloudwatch_event_rule" "s3_file_upload" {
-  name        = "s3-file-upload-rule"
-  description = "Capture file uploads to S3 bucket"
-
-  event_pattern = jsonencode({
-    source      = ["aws.s3"]
-    detail-type = ["Object Created"]
-    detail = {
-      bucket = {
-        name = [aws_s3_bucket.data_generation_bucket.id]
-      }
-    }
-  })
-}
-
 # EventBridge target
 resource "aws_cloudwatch_event_target" "lambda" {
   rule      = aws_cloudwatch_event_rule.s3_file_upload.name
